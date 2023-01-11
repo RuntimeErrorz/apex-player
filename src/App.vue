3fc1e2fcaebc5bd93204fb6918c719ee66731a8d
RuntimeError <1059171929@qq.com>
2023-01-11 23:00:35 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-dialog v-model="dialog" persistent style="width: 60vw;">
     <v-card>
       <v-card-title>
-        <span class="text-h6">选择视频源</span>
+        <span class="text-h5">选择视频源</span>
       </v-card-title>
       <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
         <v-tab v-for="item in items" :value="item">
@@ -15,12 +15,12 @@
             <v-row v-for="(item, i) in fileSrcs" :key="i">
               <v-col cols="7"> <v-file-input label="上传文件" v-model="item.files"></v-file-input>
               </v-col>
-              <v-col cols="4"><v-text-field v-model="item.label" label="该视频的标签" placeholder="1080P" file></v-text-field>
+              <v-col cols="4"><v-text-field v-model="item.label" label="清晰度" placeholder="1080P" file></v-text-field>
               </v-col>
               <v-col cols="1">
                 <v-btn icon size="large" @click="() => {
                   fileSrcs.push({
-                    files: [], type: 'video/mp4', label: ''
+                    files: [], type: '', label: ''
                   })
                 }">
                   <v-icon>mdi-plus</v-icon>
@@ -31,43 +31,66 @@
         </v-window-item>
         <v-window-item value="使用URL">
           <v-container style="margin-top: 1em;">
-            <v-row v-for="(item, i) in srcs" :key="i">
+            <v-row v-for="(item, i) in urlSrcs" :key="i">
               <v-col cols="7">
                 <v-text-field label="URL" v-model="item.src"
                   placeholder="https://vjs.zencdn.net/v/oceans.mp4"></v-text-field>
               </v-col>
-              <v-col cols="4"><v-text-field v-model="item.label" label="该视频的标签" placeholder="1080P"></v-text-field>
+              <v-col cols="4"><v-text-field v-model="item.label" label="清晰度" placeholder="1080P"></v-text-field>
               </v-col>
               <v-col cols="1">
                 <v-btn icon size="large" @click="() => {
                   srcs.push({
-                    src: '', type: 'video/mp4', label: ''
+                    src: '', type: '', label: ''
                   })
                 }">
                   <v-icon>mdi-plus</v-icon>
                 </v-btn>
               </v-col>
             </v-row>
+            <v-card-title style="margin-top: 1em;">
+              <span>供参考的视频源</span>
+            </v-card-title> <v-divider>
+            </v-divider>
+            <v-table>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    格式
+                  </th>
+                  <th class="text-left">
+                    URL
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in urls" :key="item.format">
+                  <td>{{ item.format }}</td>
+                  <td>{{ item.url }}</td>
+                </tr>
+              </tbody>
+            </v-table>
           </v-container>
         </v-window-item>
       </v-window>
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn variant="tonal" @click="() => { dialog = false, srcs = [...defaultSrcs] }">
+        <v-btn variant="tonal" @click="() => { dialog = false, srcs = reactive([...defaultSrcs]) }">
           使用默认值
         </v-btn>
         <v-btn variant="tonal" color="green-darken-1" @click="() => { dialog = false, srcs = handleFile(fileSrcs) }">
           上传文件
         </v-btn>
-        <v-btn variant="tonal" color="blue-darken-1" @click="() => { dialog = false }">
+        <v-btn variant="tonal" color="blue-darken-1"
+          @click="() => { dialog = false, srcs = addType(urlSrcs) }">
           使用URL
         </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
   <div>
-    <MyVideo :srcs="srcs" :options="options" @changeDialog="changeDialog" />
+    <MyVideo :srcs="srcs" :options="options" @resetSource="resetSource" />
   </div>
 </template>
 <script setup lang='ts'>
@@ -79,22 +102,88 @@ const dialog = ref(true)
 const tab = ref(null)
 const items = reactive(['上传文件', '使用URL'])
 
-const changeDialog = () => {
-  dialog.value = true
-  srcs = [
-    {
-      src: "",
-      type: 'video/mp4',
-      label: '',
-    },
-  ]
-  fileSrcs = [
+const filenameToMimeType = {
+  'flv': "video/x-flv",
+  'mp4': "video/mp4",
+  'mov': "video/mp4",
+  'm3u8': "application/x-mpegurl"
+}
+
+
+interface srcInterface {
+  src: string;
+  type: string;
+  label: string;
+}
+
+let srcs: Array<srcInterface>
+
+let urlSrcs = reactive([{
+  src: "",
+  type: '',
+  label: '',
+},])
+
+let fileSrcs = reactive([
+  {
+    files: [],
+    type: '',
+    label: ''
+  }
+])
+
+const urls = [
+  {
+    format: "H.264/MPEG-4 AVC",
+    url: "https://vjs.zencdn.net/v/oceans.mp4"
+  },
+  {
+    format: "HEVC/H.265",
+    url: "https://vjs.zencdn.net/v/oceans.mp4"
+  },
+  {
+    format: "MOV/QuickTime",
+    url: "https://vjs.zencdn.net/v/oceans.mp4"
+  },
+  {
+    format: "HLS",
+    url: "http://220.161.87.62:8800/hls/1/index.m3u8"
+  },
+  {
+    format: "FLV",
+    url: "https://mister-ben.github.io/videojs-flvjs/bbb.flv"
+  },
+]
+
+const addType = (srcs: Array<srcInterface>) => {
+  for (const src of srcs) {
+    const format = src.src.split(".").pop()
+    if (format === 'm3u8')
+      src.type = "application/x-mpegurl"
+    else if (format === 'flv')
+      src.type = "video/x-flv"
+    else
+      src.type = "video/mp4"
+  }
+  return srcs
+}
+
+const resetSource = () => {
+  urlSrcs = reactive([{
+    src: "",
+    type: '',
+    label: '',
+  }])
+
+  fileSrcs = reactive([
     {
       files: [],
-      type: 'video/mp4',
+      type: '',
       label: ''
     }
-  ]
+  ])
+
+  dialog.value = true
 }
 
 function handleFile(fileList: Array<{ files: Array<File>, type: string, label: string }>) {
@@ -102,14 +191,14 @@ function handleFile(fileList: Array<{ files: Array<File>, type: string, label: s
   for (const file of fileList) {
     newArray.push({
       src: URL.createObjectURL(file.files[0]),
-      type: file.type,
+      type: filenameToMimeType[<string>file.files[0].name.split('.').pop()],
       label: file.label
     })
   }
-  return newArray
+  return reactive(newArray)
 }
 
-const defaultSrcs = [{
+let defaultSrcs = [{
   src: sd,
   type: 'video/mp4',
   label: 'SD',
@@ -120,23 +209,15 @@ const defaultSrcs = [{
   label: 'HD',
 }]
 
-let srcs = reactive([
-  {
-    src: "",
-    type: 'video/mp4',
-    label: '',
-  },
-])
-
-let fileSrcs = reactive([
-  {
-    files: [],
-    type: 'video/mp4',
-    label: ''
-  }
-])
-
 const options = {
+  flvjs: {
+    mediaDataSource: {
+      isLive: false,
+      cors: true,
+      withCredentials: false,
+    },
+  },
+  autoplay: true,
   controls: true,
   customBar: {
     screenshot: true,
